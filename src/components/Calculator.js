9f39b44c53b3b5bbed085eb7763e10da13e1d8fc
Ouail-01 <ouaillaarabi@gmail.com>
2023-02-15 09:36:52 +0100

    Update the Calculator.js file

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,43 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="calculator-container">
-        <div className="calc-display">
-          0
-        </div>
-        <div className="calc-keys">
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button type="button" className="or-color">+</button>
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button type="button" className="or-color">x</button>
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button type="button" className="or-color">-</button>
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button type="button" className="or-color">+</button>
-          <button type="button" className="l-key">0</button>
-          <button type="button">.</button>
-          <button type="button" className="or-color">=</button>
-
-        </div>
+function Calculator() {
+  return (
+    <div className="calculator-container">
+      <div className="calc-display">
+        0
+      </div>
+      <div className="calc-keys">
+        <button type="button">AC</button>
+        <button type="button">+/-</button>
+        <button type="button">%</button>
+        <button type="button" className="or-color">+</button>
+        <button type="button">7</button>
+        <button type="button">8</button>
+        <button type="button">9</button>
+        <button type="button" className="or-color">x</button>
+        <button type="button">4</button>
+        <button type="button">5</button>
+        <button type="button">6</button>
+        <button type="button" className="or-color">-</button>
+        <button type="button">1</button>
+        <button type="button">2</button>
+        <button type="button">3</button>
+        <button type="button" className="or-color">+</button>
+        <button type="button" className="l-key">0</button>
+        <button type="button">.</button>
+        <button type="button" className="or-color">=</button>
 
       </div>
-    );
-  }
+
+    </div>
+  );
 }
 
 export default Calculator;
