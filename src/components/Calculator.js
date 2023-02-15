93f9aafc6d6e6d26887ea20bdf6c8b14945dda8a
Ouail-01 <ouaillaarabi@gmail.com>
2023-02-15 07:59:32 +0100

    Fix the linters

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,43 +1,43 @@
-/* eslint-disable */
-import React, {Component} from "react";
+import React, { Component } from 'react';
 
 class Calculator extends Component {
-    constructor(props) {
-        super(props);
-        this.state={};
-    }
-    render() {
-        return(
-            <div className="calculator-container">
-                <div className="calc-display">
-                    0
-                </div>
-                <div className="calc-keys">
-                    <button type="button">AC</button>
-                    <button type="button">+/-</button>
-                    <button type="button">%</button>
-                    <button type="button" className="or-color">+</button>
-                    <button type="button">7</button>
-                    <button type="button">8</button>
-                    <button type="button">9</button>
-                    <button type="button" className="or-color">x</button>
-                    <button type="button">4</button>
-                    <button type="button">5</button>
-                    <button type="button">6</button>
-                    <button type="button" className="or-color">-</button>
-                    <button type="button">1</button>
-                    <button type="button">2</button>
-                    <button type="button">3</button>
-                    <button type="button" className="or-color">+</button>
-                    <button type="button" className="l-key">0</button>
-                    <button type="button">.</button>
-                    <button type="button" className="or-color">=</button>
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-                </div>
+  render() {
+    return (
+      <div className="calculator-container">
+        <div className="calc-display">
+          0
+        </div>
+        <div className="calc-keys">
+          <button type="button">AC</button>
+          <button type="button">+/-</button>
+          <button type="button">%</button>
+          <button type="button" className="or-color">+</button>
+          <button type="button">7</button>
+          <button type="button">8</button>
+          <button type="button">9</button>
+          <button type="button" className="or-color">x</button>
+          <button type="button">4</button>
+          <button type="button">5</button>
+          <button type="button">6</button>
+          <button type="button" className="or-color">-</button>
+          <button type="button">1</button>
+          <button type="button">2</button>
+          <button type="button">3</button>
+          <button type="button" className="or-color">+</button>
+          <button type="button" className="l-key">0</button>
+          <button type="button">.</button>
+          <button type="button" className="or-color">=</button>
 
-            </div>
-        );
-    }
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default Calculator;
