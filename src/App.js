b48321cb165eaad65c1f6b78eb35af2c331bb9b2
Ouail-01 <ouaillaarabi@gmail.com>
2023-02-15 07:55:20 +0100

    Complete the App.js file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import Calculator from './components/Calculator';
+import  './App.css';
+
+function App() {
+  return (
+    <div>
+      <Calculator />
+    </div>
+  );
+}
+
+
+export default App;
