5b5d0e17fb141aacdc366cf32f63bba9f2294bab
Ouail-01 <ouaillaarabi@gmail.com>
2023-02-16 05:59:55 +0100

    Use functional component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,58 +1,55 @@
-import React, { Component } from 'react';
+import React, { useState, useCallback } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+function Calculator() {
+  const [state, setState] = useState({
 
-    onChangeHandler = (e) => {
-      const result = calculate(this.state, e.target.innerText);
-      this.setState(result);
-    }
+    total: 0,
+    next: null,
+    operation: null,
+  });
 
-    displayResult = (e) => {
-      this.setState({
-        total: e.target.innerText,
-      });
-    }
+  const onChangeHandler = useCallback((e) => {
+    const result = calculate(state, e.target.innerText);
+    setState(result);
+  },
+  [state]);
 
-    render() {
-      const { total, next, operation } = this.state;
-      return (
-        <div className="calculator-container">
-          <div className="calc-display">{total || next || operation || 0}</div>
-          <div className="calc-keys">
-            <button type="button" onClick={this.onChangeHandler}>AC</button>
-            <button type="button" onClick={this.onChangeHandler}>+/-</button>
-            <button type="button" onClick={this.onChangeHandler}>%</button>
-            <button type="button" className="or-color" onClick={this.onChangeHandler}>÷</button>
-            <button type="button" onClick={this.onChangeHandler}>7</button>
-            <button type="button" onClick={this.onChangeHandler}>8</button>
-            <button type="button" onClick={this.onChangeHandler}>9</button>
-            <button type="button" className="or-color" onClick={this.onChangeHandler}>x</button>
-            <button type="button" onClick={this.onChangeHandler}>4</button>
-            <button type="button" onClick={this.onChangeHandler}>5</button>
-            <button type="button" onClick={this.onChangeHandler}>6</button>
-            <button type="button" className="or-color" onClick={this.onChangeHandler}>-</button>
-            <button type="button" onClick={this.onChangeHandler}>1</button>
-            <button type="button" onClick={this.onChangeHandler}>2</button>
-            <button type="button" onClick={this.onChangeHandler}>3</button>
-            <button type="button" className="or-color" onClick={this.onChangeHandler}>+</button>
-            <button type="button" className="l-key" onClick={this.onChangeHandler}>0</button>
-            <button type="button" onClick={this.onChangeHandler}>.</button>
-            <button type="button" className="or-color" onClick={this.onChangeHandler}>=</button>
+  const { total, next, operation } = state;
+  return (
+    <div className="calculator-container">
+      <div className="calc-display">
+        {total}
+        {' '}
+        {operation}
+        {' '}
+        {next}
+      </div>
+      <div className="calc-keys">
+        <button type="button" onClick={onChangeHandler}>AC</button>
+        <button type="button" onClick={onChangeHandler}>%</button>
+        <button type="button" onClick={onChangeHandler}>+/-</button>
+        <button type="button" className="or-color" onClick={onChangeHandler}>÷</button>
+        <button type="button" onClick={onChangeHandler}>7</button>
+        <button type="button" onClick={onChangeHandler}>8</button>
+        <button type="button" onClick={onChangeHandler}>9</button>
+        <button type="button" className="or-color" onClick={onChangeHandler}>x</button>
+        <button type="button" onClick={onChangeHandler}>4</button>
+        <button type="button" onClick={onChangeHandler}>5</button>
+        <button type="button" onClick={onChangeHandler}>6</button>
+        <button type="button" className="or-color" onClick={onChangeHandler}>-</button>
+        <button type="button" onClick={onChangeHandler}>1</button>
+        <button type="button" onClick={onChangeHandler}>2</button>
+        <button type="button" onClick={onChangeHandler}>3</button>
+        <button type="button" className="or-color" onClick={onChangeHandler}>+</button>
+        <button type="button" className="l-key" onClick={onChangeHandler}>0</button>
+        <button type="button" onClick={onChangeHandler}>.</button>
+        <button type="button" className="or-color" onClick={onChangeHandler}>=</button>
 
-          </div>
+      </div>
 
-        </div>
-      );
-    }
+    </div>
+  );
 }
 
 export default Calculator;
