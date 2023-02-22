8f2fe2afb7ba8c80d945b7c23bdc0c4a0af66d32
Ouail-01 <ouaillaarabi@gmail.com>
2023-02-22 11:23:50 +0100

    Add the quotes file

---
+++ src/components/Quotes.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+import React from 'react';
+import "../styles/Quotes.modules.css";
+
+const Quote = () => (
+  <>
+    <p className="quotes">
+      Mathematician is not about numbers, equations, computations,
+      <br />
+      or algorithms: it is about understanding -William Paul Thurston
+    </p>
+  </>
+);
+
+export default Quote;
