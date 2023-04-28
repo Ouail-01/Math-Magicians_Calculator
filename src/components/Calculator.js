c7df9361a8a302329f833d80ef166237dbe9827e
Ouail-01 <ouaillaarabi@gmail.com>
2023-04-28 23:39:21 +0100

    Fix the linters

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import React, { useState, useCallback } from 'react';
 import calculate from '../logic/calculate';
 
@@ -19,41 +18,41 @@ function Calculator() {
   const { total, next, operation } = state;
   return (
     <>
-    <div className="calc-container">
-      <h1>Let&apos;s do some Math!</h1>
-      <div className="calculator-container">
-      <div className="calc-display">
-        {total}
-        {' '}
-        {operation}
-        {' '}
-        {next}
-      </div>
-      <div className="calc-keys">
-        <button type="button" onClick={onChangeHandler}>AC</button>
-        <button type="button" onClick={onChangeHandler}>%</button>
-        <button type="button" onClick={onChangeHandler}>+/-</button>
-        <button type="button" className="or-color" onClick={onChangeHandler}>÷</button>
-        <button type="button" onClick={onChangeHandler}>7</button>
-        <button type="button" onClick={onChangeHandler}>8</button>
-        <button type="button" onClick={onChangeHandler}>9</button>
-        <button type="button" className="or-color" onClick={onChangeHandler}>x</button>
-        <button type="button" onClick={onChangeHandler}>4</button>
-        <button type="button" onClick={onChangeHandler}>5</button>
-        <button type="button" onClick={onChangeHandler}>6</button>
-        <button type="button" className="or-color" onClick={onChangeHandler}>-</button>
-        <button type="button" onClick={onChangeHandler}>1</button>
-        <button type="button" onClick={onChangeHandler}>2</button>
-        <button type="button" onClick={onChangeHandler}>3</button>
-        <button type="button" className="or-color" onClick={onChangeHandler}>+</button>
-        <button type="button" className="l-key" onClick={onChangeHandler}>0</button>
-        <button type="button" onClick={onChangeHandler}>.</button>
-        <button type="button" className="or-color" onClick={onChangeHandler}>=</button>
+      <div className="calc-container">
+        <h1>Let&apos;s do some Math!</h1>
+        <div className="calculator-container">
+          <div className="calc-display">
+            {total}
+            {' '}
+            {operation}
+            {' '}
+            {next}
+          </div>
+          <div className="calc-keys">
+            <button type="button" onClick={onChangeHandler}>AC</button>
+            <button type="button" onClick={onChangeHandler}>%</button>
+            <button type="button" onClick={onChangeHandler}>+/-</button>
+            <button type="button" className="or-color" onClick={onChangeHandler}>÷</button>
+            <button type="button" onClick={onChangeHandler}>7</button>
+            <button type="button" onClick={onChangeHandler}>8</button>
+            <button type="button" onClick={onChangeHandler}>9</button>
+            <button type="button" className="or-color" onClick={onChangeHandler}>x</button>
+            <button type="button" onClick={onChangeHandler}>4</button>
+            <button type="button" onClick={onChangeHandler}>5</button>
+            <button type="button" onClick={onChangeHandler}>6</button>
+            <button type="button" className="or-color" onClick={onChangeHandler}>-</button>
+            <button type="button" onClick={onChangeHandler}>1</button>
+            <button type="button" onClick={onChangeHandler}>2</button>
+            <button type="button" onClick={onChangeHandler}>3</button>
+            <button type="button" className="or-color" onClick={onChangeHandler}>+</button>
+            <button type="button" className="l-key" onClick={onChangeHandler}>0</button>
+            <button type="button" onClick={onChangeHandler}>.</button>
+            <button type="button" className="or-color" onClick={onChangeHandler}>=</button>
 
-      </div>
+          </div>
 
-    </div>
-    </div>
+        </div>
+      </div>
     </>
   );
 }
