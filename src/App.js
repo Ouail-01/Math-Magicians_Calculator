4727e03cf08a1d7ef078f8aebd770bd0dafe3761
Ouail-01 <ouaillaarabi@gmail.com>
2023-05-03 11:03:29 +0100

    Add the home page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Quotes from './components/Quotes';
 import './styles/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
+        <Route exact path="/" element={<Home />} />
         <Route path="/Calculator" element={<Calculator />} />
         <Route path="/quotes" element={<Quotes />} />
       </Routes>
---
+++ src/components/Home.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Home = () => (
+  <>
+    <div className="home">
+      <h1>Welcome To Our Page!</h1>
+      <p>
+        An electronic calculator is typically a portable electronic device used to perform
+        calculations,
+        ranging from basic arithmetic to complex mathematics.
+        The first solid-state electronic calculator was created in the early 1960s.
+        Pocket-sized devices became available in the 1970s, especially after the Intel 4004,
+        the first microprocessor, was developed by Intel for the Japanese calculator company
+        Busicom.
+        Modern electronic calculators vary from cheap, give-away,
+        credit-card-sized models to sturdy desktop models with built-in printers.
+        They became popular in the mid-1970s as the incorporation of integrated circuits reduced
+        their size and cost.
+        By the end of that decade, prices had dropped to the point where a basic calculator
+        was affordable to most and
+        they became common in schools.
+      </p>
+      <p>
+        By 1970, a calculator could be made using just a few chips of low power consumption,
+        allowing portable models powered from rechargeable batteries.
+        The first handheld calculator was a 1967 prototype called Cal Tech,
+        whose development was led by Jack Kilby at Texas Instruments in a research project to
+        produce a portable calculator.
+        It could add, multiply, subtract, and divide, and its output device was a paper tape.
+        [29][30][31][32][33][34] As a result of the &ldquo;Cal-Tech&ldquo; project,
+        Texas Instruments was granted master patents on portable calculators.
+        The first commercially produced portable calculators appeared in Japan in 1970,
+        and were soon marketed around the world.
+        These included the Sanyo ICC-0081 &ldquo;Mini Calculator&ldquo;,
+        the Canon Pocketronic, and the Sharp QT-8B &ldquo;micro Compet&ldquo;.
+        The Canon Pocketronic was a development from the &ldquo;Cal-Tech&ldquo; project.
+        It had no traditional display;
+        numerical output was on thermal paper tape.
+      </p>
+    </div>
+  </>
+);
+export default Home;
